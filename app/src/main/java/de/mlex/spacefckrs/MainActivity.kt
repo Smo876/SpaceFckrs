2425f387681059d866f5cb0b543f6a9e01f0bf22
Smo876 <73527999+Smo876@users.noreply.github.com>
2024-10-06 22:10:36 +0200

    Splash API

--- app/src/main/java/de/mlex/spacefckrs/MainActivity.kt
+++ app/src/main/java/de/mlex/spacefckrs/MainActivity.kt
@@ -1,46 +1,122 @@
 package de.mlex.spacefckrs
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import de.mlex.spacefckrs.ui.elements.BottomBar
+import de.mlex.spacefckrs.ui.elements.TopBar
 import de.mlex.spacefckrs.ui.theme.SpaceFckrsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         setContent {
             SpaceFckrsTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                ScreenSpaceFckrs()
             }
         }
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Preview(showBackground = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ScreenSpaceFckrs() {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ) {
+        Scaffold(
+            modifier = Modifier
+                .background(Color.Black),
+            topBar = { TopBar() },
+            bottomBar = { BottomBar() },
+        ) {
+            Aliens()
+        }
+//        Aliens()
+    }
+}
+
+@Composable
+fun Aliens() {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 38.dp, bottom = 38.dp)
+            .background(Color.Black),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.Top
+    ) {
+        Alien(
+            color = Color.Red,
+            modifier = Modifier
+                .size(70.dp)
+                .padding(4.dp)
+        )
+        Alien(
+            color = Color.Gray,
+            modifier = Modifier
+                .size(70.dp)
+                .padding(4.dp)
+        )
+        Alien(
+            color = Color.Green,
+            modifier = Modifier
+                .size(70.dp)
+                .padding(4.dp)
+        )
+        Alien(
+            color = Color.Blue,
+            modifier = Modifier
+                .size(70.dp)
+                .padding(4.dp)
+        )
+        Alien(
+            color = Color.Green,
+            modifier = Modifier
+                .size(70.dp)
+                .padding(4.dp)
+        )
+
+    }
+}
+
+@Composable
+fun Alien(color: Color, modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier,
+        contentDescription = "Alien",
+        painter = painterResource(R.drawable.android_alien),
+        colorFilter = ColorFilter.tint(color = color)
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SpaceFckrsTheme {
-        Greeting("Android")
-    }
-}
+
+
+
+
---
+++ app/src/main/java/de/mlex/spacefckrs/ui/elements/TopBar.kt
@@ -0,0 +1,40 @@
+package de.mlex.spacefckrs.ui.elements
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TopBar() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Black),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier.padding(4.dp),
+            text = "SCORE: 0000",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+        )
+        Text(
+            text = "LEVEL: 001",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+        )
+    }
+}

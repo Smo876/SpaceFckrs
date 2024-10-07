bfd47f85637bf4e22a086db8cc8d1ffb67ad4bc6
Smo876 <73527999+Smo876@users.noreply.github.com>
2024-10-07 13:55:40 +0200

    refactor folder structure

--- app/src/main/java/de/mlex/spacefckrs/MainActivity.kt
+++ app/src/main/java/de/mlex/spacefckrs/MainActivity.kt
@@ -7,28 +7,23 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import de.mlex.spacefckrs.ui.elements.BottomBar
+import de.mlex.spacefckrs.ui.elements.TopBar
 import de.mlex.spacefckrs.ui.theme.SpaceFckrsTheme
 
 class MainActivity : ComponentActivity() {
@@ -51,14 +46,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ScreenSpaceFckrs() {
     Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = Color.Black
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
     ) {
         Scaffold(
-            modifier = Modifier
-                .background(Color.Black),
             topBar = { TopBar() },
-            bottomBar = { TopBar() },
+            bottomBar = { BottomBar() },
         ) {
             Aliens()
         }
@@ -71,7 +65,8 @@ fun Aliens() {
     Row(
         modifier = Modifier
             .fillMaxSize()
-            .padding(top = 30.dp, bottom = 30.dp),
+            .padding(top = 38.dp, bottom = 38.dp)
+            .background(Color.Black),
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.Top
     ) {
@@ -117,57 +112,6 @@ fun Alien(color: Color, modifier: Modifier = Modifier) {
         painter = painterResource(R.drawable.android_alien),
         colorFilter = ColorFilter.tint(color = color)
     )
-}
-
-@Composable
-fun GameOverBox() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
-
-    ) {
-        ScreenSpaceFckrs()
-        Spacer(
-            modifier = Modifier
-                .matchParentSize()
-                .fillMaxSize()
-                .background(color = Color.DarkGray.copy(alpha = .8f))
-        )
-        Text(
-            text = "GAME OVER",
-            fontSize = 50.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .fillMaxWidth()
-
-        )
-    }
-}
-
-@Composable
-fun TopBar() {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(
-            text = "SCORE: 0000",
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-        )
-        Text(
-            text = "LEVEL: 001",
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-        )
-    }
 }
 
 

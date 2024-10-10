f8356a3bdbc2cd2966b649a6295b2a18ae8def06
Smo876 <73527999+Smo876@users.noreply.github.com>
2024-10-10 22:02:05 +0200

    viewModel

--- app/src/main/java/de/mlex/spacefckrs/ui/elements/DrawAlien.kt
+++ app/src/main/java/de/mlex/spacefckrs/ui/elements/DrawAlien.kt
@@ -3,10 +3,7 @@ package de.mlex.spacefckrs.ui.elements
 import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
-import de.mlex.spacefckrs.R
 
 @Composable
 fun DrawAlien(type: Int, modifier: Modifier = Modifier) {
--- app/src/main/java/de/mlex/spacefckrs/ui/elements/PlayField.kt
+++ app/src/main/java/de/mlex/spacefckrs/ui/elements/PlayField.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import de.mlex.spacefckrs.Alien
 

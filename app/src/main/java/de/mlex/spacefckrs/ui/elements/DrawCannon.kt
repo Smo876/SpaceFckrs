ee0b5418f0e468779ba0ded3b872363da8df412e
Smo876 <73527999+Smo876@users.noreply.github.com>
2024-12-01 20:04:17 +0100

    draw cannon refactored

--- app/src/main/java/de/mlex/spacefckrs/ui/elements/DrawCannon.kt
+++ app/src/main/java/de/mlex/spacefckrs/ui/elements/DrawCannon.kt
@@ -3,16 +3,16 @@ package de.mlex.spacefckrs.ui.elements
 import androidx.compose.foundation.Image
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import de.mlex.spacefckrs.CannonState
 import de.mlex.spacefckrs.R
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlin.time.Duration.Companion.seconds
@@ -24,30 +24,34 @@ fun DrawCannon(
     onShoot: (Int) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    var isShooting by remember { mutableStateOf(false)}
+    val isShooting = remember { mutableStateOf(false) }
     val coroutine = rememberCoroutineScope()
     IconButton(
         modifier = modifier,
         enabled = (cannonState == CannonState.IsReady),
         onClick = {
             onShoot(cannon)
-            isShooting = true
-            coroutine.launch {
-                delay(0.5.seconds)
-                isShooting = false
-            }
+            coroutine.animateCannon(isShooting)
         }) {
         Image(
             contentDescription = "Cannon",
             painter = painterResource(
                 when {
-                    cannonState == CannonState.WasDestroyed ->  R.drawable.sf_destroyedcannon
-                    isShooting -> R.drawable.sf_firingcannon
+                    cannonState == CannonState.WasDestroyed -> R.drawable.sf_destroyedcannon
+                    isShooting.value -> R.drawable.sf_firingcannon
                     else -> R.drawable.sf_cannon
                 }
             ),
             contentScale = ContentScale.Fit,
             alpha = 1.0f,
         )
+    }
+}
+
+private fun CoroutineScope.animateCannon(isShooting: MutableState<Boolean>) {
+    isShooting.value = true
+    launch {
+        delay(0.5.seconds)
+        isShooting.value = false
     }
 }

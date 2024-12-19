d8f7a9292651e35d1e7b55986b2936f1541e8d5f
Smo876 <73527999+Smo876@users.noreply.github.com>
2024-12-19 22:38:49 +0100

    refactor StateMachine

--- app/src/main/java/de/mlex/spacefckrs/ui/elements/DrawAnimation.kt
+++ app/src/main/java/de/mlex/spacefckrs/ui/elements/DrawAnimation.kt
@@ -1,15 +1,10 @@
 package de.mlex.spacefckrs.ui.elements
 
-import android.util.Log
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
